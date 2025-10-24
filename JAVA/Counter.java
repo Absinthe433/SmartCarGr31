d91ebc9ffb432cd5546d1b0ce326e20e4542be89
Absinthe433 <2426927563@qq.com>
2025-10-24 22:14:43 +0800

    Delete JAVA directory

--- JAVA/Counter.java
+++
@@ -1,9 +0,0 @@
-public class Counter extends Thread {
-    @Override
-    public void run() {
-        // 从10计数到500并输出
-        for (int i = 10; i <= 500; i++) {
-            System.out.println("Counter: " + i);
-        }
-    }
-}
